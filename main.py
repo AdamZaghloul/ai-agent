2aa9a602edc803f0086c00fd63c9d2a7e1d0867a
AdamZaghloul <adam.zaghloul95@gmail.com>
2025-06-11 20:20:32 -0400

    Added calcuator app for AI agent to work on.

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    verbose = False
+
+    if len(sys.argv) < 2:
+        print("No prompt provided.")
+        os._exit(1)
+    
+    if len(sys.argv) > 2:
+        if sys.argv[2] == "--verbose":
+            verbose = True
+    
+    prompt = sys.argv[1]
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
 
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        model='gemini-2.0-flash-001', contents=messages
     )
+
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if verbose:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 main() 
