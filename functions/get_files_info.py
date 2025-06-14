6646cb0bb105ff1336eced0dab78fb28397b5ffe
AdamZaghloul <adam.zaghloul95@gmail.com>
2025-06-13 20:59:55 -0400

    started get_files_info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,3 +3,8 @@ def get_files_info(working_directory, directory=None):
         raise Exception(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
     if !os.path.isdir(directory):
         raise Exception(f'Error: "{directory}" is not a directory')
+
+    paths = os.listdir(directory)
+
+    for path in paths:
+        
