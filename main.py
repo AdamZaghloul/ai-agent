5097fc09186d0aca69e905fbfec7d8d22bb1fbbd
AdamZaghloul <adam.zaghloul95@gmail.com>
2025-10-21 09:12:01 -0400

    added system prompt.

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     verbose = False
+    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 
     if len(sys.argv) < 2:
         print("No prompt provided.")
@@ -26,7 +27,9 @@ def main():
     ]
 
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=messages
+        model='gemini-2.0-flash-001', 
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
 
     print(response.text)
