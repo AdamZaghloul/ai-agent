a82386ae031e7ba2fb3bccb406a133fe6fd804c0
AdamZaghloul <adam.zaghloul95@gmail.com>
2025-10-21 20:07:55 -0400

    added get_files_info to function calls.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 def main():
 
@@ -10,7 +11,15 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     verbose = False
-    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
 
     if len(sys.argv) < 2:
         print("No prompt provided.")
@@ -26,11 +35,24 @@ def main():
         types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
 
+    available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions], 
+            system_instruction=system_prompt
+        ),
     )
+
+    if response.function_calls is not None:
+        for call in response.function_calls:
+            print(f"Calling function: {call.name}({call.args})")
 
     print(response.text)
 
